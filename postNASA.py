801827d4555d657e58e11a16c20ff384fecd218f
Adam Aguiar <adamaguiar10@gmail.com>
2022-07-07 16:14:00 +0000

    Literally added a tab

--- postNASA.py
+++ postNASA.py
@@ -3,8 +3,6 @@ import pprint
 import json
 from datetime import date
 
-# Add exception if file does not exist or no string in file
-
 
 def day_in_month(day, month):
     if month in (1, 3, 5, 7, 8, 10, 12):
@@ -63,7 +61,7 @@ def get_info(ids):
         response = requests.get(url)
         data = response.json()
         if data['is_potentially_hazardous_asteroid']:
-            print(data['name'].upper() + 'IS A POTENTIALLY HAZARDOUS ASTEROID')
+            print(data['name'].upper() + '\tIS A POTENTIALLY HAZARDOUS ASTEROID')
         print(data['name'] + '\t' + data['nasa_jpl_url'])
 
 
